1c102fa9ddb5ea1b05e2c40d451b856160cc6e4d
Clukva <yura4-1-1@mail.ru>
2021-08-07 23:37:40 +0300

    feat: solution task morse

--- src/index.js
+++ src/index.js
@@ -38,7 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr =[]
+    let pats =[]
+    let array= arr.concat(expr)
+    let morzeCode = array.map(element =>{
+    
+        pats =  element.split(/(.{10})/).filter(O=>O)
+     
+         for(let i=0; i < pats.length; i++) {
+            pats[i] = pats[i].replace(/11/gm, '-')
+            pats[i] = pats[i].replace(/10/gm, '.')
+            pats[i] = pats[i].replace(/\*+/gm, ' ')
+            pats[i] = pats[i].replace(/0+/gm, ' ').trim();
+            pats[i] = pats[i].replace(/ /gm, '6').trim().split(6)
+        } 
+    })
+    
+    let newArr = [];
+    for (let i = 0; i < pats.length; i++){
+        newArr+=pats[i]+' '
+    }
+    let n =[]
+    let j = n.concat(...pats)
+    
+    let newArre = [];
+    for (let i = 0; i < j.length; i++) {
+        if (j[i] === "") {
+            newArre.push(" ");
+        }
+    
+        for (const letter in MORSE_TABLE) {
+            if (letter === j[i]) {
+              let m =  newArre.push(MORSE_TABLE[letter]);
+            }
+        }
+    }
+    
+    return newArre.join('')
 }
 
 module.exports = {
